f28924247a2d85f443008d3d34745c536ecbf893
Karthick <mail4karthickr@gmail.com>
2025-05-05 17:53:59 +0530

    updated

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "RestaurantFinder",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -18,4 +21,5 @@ let package = Package(
             name: "RestaurantFinder"),
 
     ]
-)
+)// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
--- Sources/RestaurantFinder/RestaurantFinder.swift
+++ Sources/RestaurantFinder/RestaurantFinder.swift
@@ -1,2 +1,31 @@
-// The Swift Programming Language
+import SwiftUI
+
+// A public view for displaying a restaurant finder interface
+public struct RestaurantFinderView: View {
+    public init() {}
+
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Restaurant Finder")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("Find the best restaurants near you!")
+                .font(.headline)
+
+            Button(action: {
+                print("Search for Restaurants")
+            }) {
+                Text("Search")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.green)
+                    .cornerRadius(8)
+            }
+        }
+        .padding()
+    }
+}// The Swift Programming Language
 // https://docs.swift.org/swift-book
